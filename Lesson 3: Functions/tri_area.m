94ebbac335a30e6c8ec2bfbd0e157184f704793a
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-06-12 16:40:57 -0700

    Create tri_area.m

---
+++ Lesson 3: Functions/tri_area.m
@@ -0,0 +1,5 @@
+%A Simple Function
+%Write a function called tri_area that returns the area of a triangle with base b and height h, where b and h are input arguments of the function in that order.
+function a = tri_area(b,h)
+    a = 0.5*b*h
+end
