d736647ee706c2d20126fe69c20049485a8d2561
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-06-13 11:04:11 -0700

    Create guess_my_number.m

---
+++ Lesson 5: Selection/guess_my_number.m
@@ -0,0 +1,7 @@
+%learn to use if, elseif, else statements in MATLAB
+function guess_my_number(x)
+    if x==2||x==3
+        fprintf('you guessed the correct number!\n');
+    else 
+        fprintf('wrong number bro\n');
+end
