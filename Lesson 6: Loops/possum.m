4e3cdad3d8fab47c49be3210f8c9e5c0a8bc5aef
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-06-17 11:02:46 -0700

    Create possum.m

---
+++ Lesson 6: Loops/possum.m
@@ -0,0 +1,9 @@
+function [n total] = possum(limit)
+    total = 0;
+    n = 0;
+    while total <= limit
+        n = n+1;
+        total = total + n;
+    end
+    fprintf('sum: %d count: %d', total, n);
+end
