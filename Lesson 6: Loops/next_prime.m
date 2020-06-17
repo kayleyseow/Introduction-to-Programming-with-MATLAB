4a21b24f43422dab1e2c05fe2a40107cf31e725e
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-06-17 11:10:49 -0700

    Create next_prime.m

---
+++ Lesson 6: Loops/next_prime.m
@@ -0,0 +1,18 @@
+%Practice while-loops
+%Write a function called next_prime that takes a scalar positive integer input n. Use a while-loop to find and return k, the smallest prime number that is greater than n. Feel free to use the built-in isprime function. Here are some example runs:
+%>> next_prime(2)
+%ans =
+%     3
+%>> next_prime(8)
+%ans =
+%    11
+%>> next_prime(12345678)
+%ans =
+%    12345701
+function k = next_prime(x)
+    temp = x+1;
+    while isprime(temp) ~= 1
+        temp = temp+1;
+    end
+    k=temp;
+end
